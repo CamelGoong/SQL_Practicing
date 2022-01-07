dd9b09be1e9c633c9782bc1f73f787386884efef
CamelGoong <goong0405@gmail.com>
2022-01-07 23:21:01 +0900

    Add files via upload

---
+++ 1-3. Workbench 살펴보기.sql
@@ -0,0 +1,15 @@
+/*
+- Ctrl + T: 새로운 쿼리창 열기
+- Ctrl + Enter: 쿼리 실행
+- Ctrl + S: 쿼리 저장
+** MAC OS의 경우 Ctrl -> command
+*/
+
+/*데이터 베이스 생성*/
+CREATE DATABASE TEST;
+
+/*해당 데이터베이스 사용*/
+USE TEST;
+
+/*데이터 베이스 삭제*/
+DROP DATABASE TEST;
